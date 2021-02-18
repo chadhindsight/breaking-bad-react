6ab84975ac0d73e6c794bb45f68cdac5dbd03aca
Chad Hinds <chadrickj8@gmail.com>
2021-02-18 11:28:20 -0500

    Add Ui elements like a spinner and search component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import Header from './component/ui/Header';
+import Header from './components/ui/Header';
+import Search from './components/ui/Search';
+import CharacterGrid from './components/characters/CharacterGrid'
+import axios from 'axios';
 import './App.css';
 
 function App() {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    // You cant put an async function directly on useEffect so that's why we have fetchItems 
+    const fetchItems = async () => {
+      setIsLoading(true)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      )
+
+      console.log(result.data)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [query])
+
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }

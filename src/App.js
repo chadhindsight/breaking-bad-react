d3f87065791e8a18c14b67c8eb63cc962f793f4b
Chad Hinds <chadrickj8@gmail.com>
2021-02-28 10:18:42 -0500

    Pass isLoading down to the CharacterGrid component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Header from './components/ui/Header';
-import Search from './components/ui/Search';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Header from './components/ui/Header'
 import CharacterGrid from './components/characters/CharacterGrid'
-import axios from 'axios';
-import './App.css';
+import Search from './components/ui/Search'
+import './App.css'
 
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
-import { Context } from './context'
-import { ContextController } from "./context";
+  useEffect(() => {
+    const fetchItems = async () => {
+      setIsLoading(true)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      )
 
+      console.log(result.data)
 
-function App() {
-  const { value, value2, value3 } = useContext(Context)
+      setItems(result.data)
+      setIsLoading(false)
+    }
 
-  const [items] = useContext(value)
-  const [isLoading] = useContext(value2)
-  const [query, setQuery] = useContext(value3)
+    fetchItems()
+  }, [query])
 
   return (
-    <div className="container">
+    <div className='container'>
       <Header />
       <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
-  );
+  )
 }
 
 export default App;
+
+// Update soon come

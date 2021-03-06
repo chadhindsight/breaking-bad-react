69c490822af8986e17791e7e6c22d3e0e4b3d8c7
Chad Hinds <chadrickj8@gmail.com>
2021-03-06 11:27:00 -0500

    Update index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ContextController } from "./context";
-import { Context } from './context'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <ContextController>
-      <App />
-    </ContextController>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass a function!
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
